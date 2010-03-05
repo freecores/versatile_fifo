f1acd9f4662a228ca493203a99a7c719d1aa77d8
unneback <unneback@be8f074f-447e-4b13-a22a-9ccc50b0e47f>
2010-03-05 21:38:01 +0000

    added DFF SR

--- rtl/verilog/versatile_fifo_async_cmp.v
+++ rtl/verilog/versatile_fifo_async_cmp.v
@@ -1,3 +1,45 @@
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+////  Versatile counter                                           ////
+////                                                              ////
+////  Description                                                 ////
+////  Versatile counter, a reconfigurable binary, gray or LFSR    ////
+////  counter                                                     ////
+////                                                              ////
+////  To Do:                                                      ////
+////   - add LFSR with more taps                                  ////
+////                                                              ////
+////  Author(s):                                                  ////
+////      - Michael Unneback, unneback@opencores.org              ////
+////        ORSoC AB                                              ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Copyright (C) 2009 Authors and OPENCORES.ORG                 ////
+////                                                              ////
+//// This source file may be used and distributed without         ////
+//// restriction provided that this copyright statement is not    ////
+//// removed from the file and that any derivative work contains  ////
+//// the original copyright notice and the associated disclaimer. ////
+////                                                              ////
+//// This source file is free software; you can redistribute it   ////
+//// and/or modify it under the terms of the GNU Lesser General   ////
+//// Public License as published by the Free Software Foundation; ////
+//// either version 2.1 of the License, or (at your option) any   ////
+//// later version.                                               ////
+////                                                              ////
+//// This source is distributed in the hope that it will be       ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
+//// PURPOSE.  See the GNU Lesser General Public License for more ////
+//// details.                                                     ////
+////                                                              ////
+//// You should have received a copy of the GNU Lesser General    ////
+//// Public License along with this source; if not, download it   ////
+//// from http://www.opencores.org/lgpl.shtml                     ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+
 module versatile_fifo_async_cmp ( wptr, rptr, fifo_empty, fifo_full, wclk, rclk, rst );
 
    parameter ADDR_WIDTH = 4;   
@@ -12,13 +54,15 @@ module versatile_fifo_async_cmp ( wptr, rptr, fifo_empty, fifo_full, wclk, rclk,
    parameter going_full  = 1'b1;
    
    input [N:0]  wptr, rptr;   
-   output reg	fifo_empty, fifo_full;
+   output reg	fifo_empty;
+   output       fifo_full;
    input 	wclk, rclk, rst;   
    
    reg 	direction, direction_set, direction_clr;
    
    wire async_empty, async_full;
-   reg 	fifo_full2, fifo_empty2;   
+   wire fifo_full2;
+   reg  fifo_empty2;   
    
    // direction_set
    always @ (wptr[N:N-1] or rptr[N:N-1])
@@ -42,16 +86,26 @@ module versatile_fifo_async_cmp ( wptr, rptr, fifo_empty, fifo_full, wclk, rclk,
 	 {Q1,Q4} : direction_clr <= 1'b1;
 	 default : direction_clr <= 1'b0;
        endcase
-     
+
+`ifndef GENERATE_DIRECTION_AS_LATCH
+    dff_sr dff_sr_dir( .aclr(direction_clr), .aset(direction_set), .clock(1'b1), .data(1'b1), .q(direction));
+`endif
+
+`ifdef GENERATE_DIRECTION_AS_LATCH
    always @ (posedge direction_set or posedge direction_clr)
      if (direction_clr)
        direction <= going_empty;
      else
        direction <= going_full;
+`endif
 
    assign async_empty = (wptr == rptr) && (direction==going_empty);
    assign async_full  = (wptr == rptr) && (direction==going_full);
 
+    dff_sr dff_sr_empty0( .aclr(rst), .aset(async_full), .clock(wclk), .data(async_full), .q(fifo_full2));
+    dff_sr dff_sr_empty1( .aclr(rst), .aset(async_full), .clock(wclk), .data(fifo_full2), .q(fifo_full));
+
+/*
    always @ (posedge wclk or posedge rst or posedge async_full)
      if (rst)
        {fifo_full, fifo_full2} <= 2'b00;
@@ -59,11 +113,11 @@ module versatile_fifo_async_cmp ( wptr, rptr, fifo_empty, fifo_full, wclk, rclk,
        {fifo_full, fifo_full2} <= 2'b11;
      else
        {fifo_full, fifo_full2} <= {fifo_full2, async_full};
-
+*/
    always @ (posedge rclk or posedge async_empty)
      if (async_empty)
        {fifo_empty, fifo_empty2} <= 2'b11;
      else
        {fifo_empty,fifo_empty2} <= {fifo_empty2,async_empty};   
-   
+
 endmodule // async_comp
